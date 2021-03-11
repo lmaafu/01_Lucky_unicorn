cc8cf61694d2fb1e6521648bf259760c8b7ce683
lmaafu <78514223+lmaafu@users.noreply.github.com>
2021-03-11 13:10:41 +1300

    00 lu base

--- 00_LU_Base_V1.py
+++ 00_LU_Base_V1.py
@@ -18,9 +18,9 @@ def yes_no(question):
 
 
 def instructions():
-    print(" **** How to play **** ")
+    statement_generator("How To Play", "*")
     print()
-    print("The rules of the game go here")
+    print("Pick a number between 1 and 10")
     print()
     return""
 
@@ -76,50 +76,50 @@ rounds_played = 0
 play_again = input("Press <Enter> to play...").lower()
 while play_again == "":
 
-        # increase # of rounds played
+    # increase # of rounds played
     rounds_played += 1
 
-# Print round number
-print("*** Round #{} ***".format(rounds_played))
+    # Print round number
+    print("*** Round #{} ***".format(rounds_played))
 
-chosen_num = random.randint(1, 100)
+    chosen_num = random.randint(1, 100)
 
-# Adjust balance
-# If the random # id between 1 and 5
-# user gets a unicorn (add $4 to balance
-if 1 <= chosen_num <= 5:
-    chosen = "unicorn"
-    decoration = "U"
-    balance += 4
-    # if the random # is between 6 and 36
-    # user gets a donkey (subtract $1 from balance)
-elif 6 <= chosen_num <= 36:
-    chosen = "donkey"
-    prize_decoration = "D"
-    balance -= 1
-    # The token is either a horse or zebra ....
-    # in both cases, subtract $0.50 from the balance
-else:
-    if chosen_num % 2 == 0:
-        chosen = "horse"
-        prize_decorations = "H"
-        # otherwise set it to a zebra
+    # Adjust balance
+    # If the random # id between 1 and 5
+    # user gets a unicorn (add $4 to balance
+    if 1 <= chosen_num <= 3:
+        chosen = "unicorn"
+        prize_decoration = "!"
+        balance += 4
+        # if the random # is between 6 and 36
+        # user gets a donkey (subtract $1 from balance)
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        prize_decoration = "D"
+        balance -= 1
+        # The token is either a horse or zebra ....
+        # in both cases, subtract $0.50 from the balance
     else:
-        chosen = "zebra"
-        prize_decoration = "Z"
-    balance -= 0.5
-outcome = "You got a {}. Your balance is " \
-          "${:.2f}".format(chosen, balance)
-statement_generator(outcome, prize_decoration)
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+            prize_decoration = "H"
+            # otherwise set it to a zebra
+        else:
+            chosen = "zebra"
+            prize_decoration = "Z"
+        balance -= 0.5
+    outcome = "You got a {}. Your balance is " \
+              "${:.2f}".format(chosen, balance)
+    statement_generator(outcome, prize_decoration)
 
-if balance < 1:
-    # If balance is to low, exit the game and
-    # output a suitable message
-        play_again = "xxx"
-        print("Sorry you have run out of money ")
-else:
-    play_again = input("Press enter to pay again or 'xxx' to quit")
+    if balance < 1:
+        # If balance is to low, exit the game and
+        # output a suitable message
+            play_again = "xxx"
+            print("Sorry you have run out of money ")
+    else:
+        play_again = input("Press enter to pay again or 'xxx' to quit")
 
-print()
-print("Final balance", balance)
+    print()
+print("Final balance is  ${:.2f}".format(balance))
 
